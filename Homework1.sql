708cf3227039337200c94d6e8f1e2e5fdcf64407
Annazio <124679541+Annazio@users.noreply.github.com>
2023-05-29 20:55:04 +0200

    Homework1

---
+++ Homework1.sql
@@ -0,0 +1,14 @@
+use hr;
+
+select first_name, last_name from employees where department_id = 90;
+
+select first_name, last_name from employees where salary > 5000;
+
+select last_name from employees where last_name like "L%";
+
+select * from departments where location_id = 1700;
+
+select city from locations where country_id = "US";
+
+
+
